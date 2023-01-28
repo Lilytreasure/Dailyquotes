f1ca789696eb2e221a7311da4d65cda5d0c906c7
Lilytreasure <lilyngure@gmail.com>
2023-01-28 15:35:16 +0300

    set up

---
+++ app/src/main/java/com/example/quotes/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.quotes
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    //An android application that fetches quotes form an api
+
+
+
+
+
+
+
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
